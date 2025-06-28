60745c5baf89e00c4fa63798eb12dccd96823ab8
MohammedA <alshamim846@gmail.com>
2025-06-28 21:58:18 +0200

    Update to Neural Netz and Trainer.

--- app/src/main/java/model/TrainerMain.java
+++ app/src/main/java/model/TrainerMain.java
@@ -1,48 +1,7 @@
-<<<<<<< HEAD
 package model;
 import java.util.*;
-=======
-class Trainer{
-    private NeuralNetwork neuralNetwork;
-    private double learningRate;
-    
-    private int epochs;
->>>>>>> 298e964c612efd3f953e6a1823ebb990bea82970
 
 import model.Trainer;
-
-<<<<<<< HEAD
-=======
-    public Trainer(NeuralNetwork neuralNetwork, double learningRate, int epochs,double[][] inputData, double[][] targetData) {
-        this.neuralNetwork = neuralNetwork;
-        this.learningRate = learningRate;
-        this.epochs = epochs;
-        this.inputData = inputData;
-        this.targetData = targetData;
-    }
-
-
-    public void back(double[] inputs,double[] optimalOutputs){
-        double [] outputs = neuralNetwork.feedforward(inputs);
-        double [] outputErrors = MathFunctions.meanSquaredError(optimalOutputs, outputs);
-        double[] hiddenOutput = neuralNetwork.getHiddenOutput();
-
-        for (int i = 0; i < weightsHiddenOutput.length; i++) {
-            for (int j = 0; j < weightsHiddenOutput[i].length; j++) {
-                weightsHiddenOutput[i][j] += learningRate * outputErrors[i] * hiddenOutput[j];
-            }
-        }
-        neuralNetwork.setWeightsHiddenOutput(weightsHiddenOutput);
-
-        double[] biasOutput = neuralNetwork.getBiasOutput();
-        for (int i = 0; i < biasOutput.length; i++) {
-            biasOutput[i] += learningRate * outputErrors[i];
-        }
-        neuralNetwork.setBiasOutput(biasOutput);
-    }
-    
-}
->>>>>>> 298e964c612efd3f953e6a1823ebb990bea82970
 public class TrainerMain{
     public static void main(String[] args) {
         double[][] inputs = randomMatrix(3,3);
